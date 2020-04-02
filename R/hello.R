7b699f3f277cf6301de3cd8dbf96ea4bc621bd47
Jongchan Lee <jclee@JongchannoMacBook-Air.local>
2020-04-03 00:33:06 +0900

    first commit

---
+++ R/hello.R
@@ -0,0 +1,9 @@
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+Michi_Eki_data<- read.csv("Michi_Eki_data.csv", fileEncoding="cp932")
+iris.df<-iris
+iris.list<-iris3
+iris.list1<-iris3
+# aa<-iris3
---
+++ test.R
@@ -0,0 +1,4 @@
+remove.packages(pkgs = "jcsample")
+devtools::install_local("/Users/jclee/jcsample")
+devtools::install_github("luka3117/jcsample")
+library(jcsample)
