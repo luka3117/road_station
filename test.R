bc19f8513c8eb995c567d335e6b8369cca37b578
Jongchan Lee <jclee@JongchannoMacBook-Air.local>
2020-04-05 01:53:08 +0900

    令和2年4月5日

--- test.R
+++ test.R
@@ -1,7 +1,12 @@
-remove.packages("RoadStation")
+# remove.packages("RoadStation")
 
-devtools::install_local("/Users/jclee/road_station")
+# devtools::install_local("/Users/jclee/road_station")
 devtools::install_github("luka3117/road_station")
+
+library(RoadStation)
+
+help(Michi_Eki_data)
+help(道の駅)
 
 RoadStation::道の駅
 
